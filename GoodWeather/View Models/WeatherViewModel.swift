55d58038f92a9ca928fd3a549c12655f22129b3a
Mohammad Azam <azamsharp@gmail.com>
2020-08-13 11:28:42 -0500

    loading state and refactoring

--- GoodWeather/View Models/WeatherViewModel.swift
+++ GoodWeather/View Models/WeatherViewModel.swift
@@ -8,10 +8,18 @@
 
 import Foundation
 
+enum LoadingState {
+    case none
+    case loading
+    case success
+    case failed
+}
+
 class WeatherViewModel: ObservableObject {
     
     @Published private var weather: Weather?
     @Published var message: String = ""
+    @Published var loadingState: LoadingState = .none
     
     var temperature: Double {
         guard let temp = weather?.temp else {
@@ -20,22 +28,35 @@ class WeatherViewModel: ObservableObject {
         return temp
     }
     
+    var humidity: Double {
+        guard let humidity = weather?.humidity else {
+            return 0.0
+        }
+        return humidity
+    }
+    
     func fetchWeather(city: String) {
         
         guard let city = city.escaped() else {
-            fatalError("URL is invalid!")
+            DispatchQueue.main.async {
+                self.message = "City is incorrect"
+            }
+            return
         }
+        
+        self.loadingState = .loading
         
         WeatherService().getWeather(city: city) { result in
             switch result {
                 case .success(let weather):
                     DispatchQueue.main.async {
                         self.weather = weather
+                        self.loadingState = .success
                     }
                 case .failure(_ ):
                     DispatchQueue.main.async {
                         self.message = "Unable to find weather"
-                        self.weather = nil
+                        self.loadingState = .failed
                 }
             }
         }
