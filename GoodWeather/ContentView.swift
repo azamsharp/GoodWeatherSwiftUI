3fb3c3cf2baaa0de2bca3d29626358aa8b7c8b68
Mohammad Azam <azamsharp@gmail.com>
2020-08-12 19:51:20 -0500

    loading state

--- GoodWeather/ContentView.swift
+++ GoodWeather/ContentView.swift
@@ -10,8 +10,12 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @ObservedObject private var weatherVM = WeatherViewModel()
+    @ObservedObject var weatherVM: WeatherViewModel
     @State private var city: String = ""
+    
+    init(weatherVM: WeatherViewModel = WeatherViewModel()) {
+        self.weatherVM = weatherVM
+    }
     
     var body: some View {
         
@@ -24,15 +28,82 @@ struct ContentView: View {
             }).textFieldStyle(RoundedBorderTextFieldStyle())
                
             Spacer()
-            Text("\(weatherVM.temperature)")
+            if self.weatherVM.loadingState == .loading {
+                LoadingView()
+            } else if self.weatherVM.loadingState == .success {
+                WeatherView(temperature: self.weatherVM.temperature, humidity: self.weatherVM.humidity)
+            } else if self.weatherVM.loadingState == .failed {
+                ErrorView(message: self.weatherVM.message)
+            }
             Spacer()
-            Text(weatherVM.message)
+ 
         }.padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        
+        let vm = WeatherViewModel()
+        vm.loadingState = .none
+        vm.message = "Unable to load weather!"
+        return ContentView(weatherVM: vm)
     }
+}
+
+struct WeatherView: View {
+    
+    let temperature: Double
+    let humidity: Double
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            Text("\(temperature)")
+                .font(.largeTitle)
+                .foregroundColor(Color.white)
+            Text("\(humidity)")
+                .foregroundColor(Color.white)
+                .opacity(0.7)
+        }
+        .padding()
+        .frame(width:300, height: 150)
+        .background(Color.blue)
+        .clipShape(RoundedRectangle(cornerRadius: 8.0, style: .continuous))
+    
+    }
+}
+
+struct LoadingView: View {
+    var body: some View {
+        VStack {
+            Text("Loading your amazing weather!")
+                .font(.body)
+                .foregroundColor(Color.white)
+          
+        }
+        .padding()
+        .frame(width:300, height: 150)
+        .background(Color.orange)
+        .clipShape(RoundedRectangle(cornerRadius: 8.0, style: .continuous))
+    
+    }
+}
+
+struct ErrorView: View {
+    
+    let message: String
+    
+   var body: some View {
+       VStack {
+           Text(message)
+               .font(.body)
+               .foregroundColor(Color.white)
+         
+       }
+       .padding()
+       .frame(width:300, height: 150)
+       .background(Color.red)
+       .clipShape(RoundedRectangle(cornerRadius: 8.0, style: .continuous))
+   
+   }
 }
--- GoodWeather/View Models/WeatherViewModel.swift
+++ GoodWeather/View Models/WeatherViewModel.swift
@@ -8,10 +8,18 @@
 
 import Foundation
 
+enum LoadingState {
+    case none
+    case loading
+    case success
+    case failed
+}
+
 class WeatherViewModel: ObservableObject {
     
     @Published private var weather: Weather?
     @Published var message: String = ""
+    @Published var loadingState: LoadingState = .none
     
     var temperature: Double {
         guard let temp = weather?.temp else {
@@ -20,10 +28,18 @@ class WeatherViewModel: ObservableObject {
         return temp
     }
     
+    var humidity: Double {
+           guard let humidity = weather?.humidity else {
+               return 0.0
+           }
+           return humidity
+       }
+    
     func fetchWeather(city: String) {
         
         guard let city = city.escaped() else {
-            fatalError("URL is invalid!")
+            self.message = "City is incorrect!"
+            return
         }
         
         WeatherService().getWeather(city: city) { result in
@@ -31,11 +47,12 @@ class WeatherViewModel: ObservableObject {
                 case .success(let weather):
                     DispatchQueue.main.async {
                         self.weather = weather
+                        self.loadingState = .success
                     }
                 case .failure(_ ):
                     DispatchQueue.main.async {
                         self.message = "Unable to find weather"
-                        self.weather = nil
+                        self.loadingState = .failed
                 }
             }
         }
